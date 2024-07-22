20e3fe3daf4c1dd46c35ac1b20b0f8cfbd382c43
lemonowl <tcm.panika@gmail.com>
2024-07-22 18:49:53 +0300

    scripts

--- docker_clean.sh
+++
@@ -1,4 +0,0 @@
-docker rm %container%
-docker volume prune
-docker rmi %image%
-docker system df
--- docserver_runner.rb
+++ docserver_runner.rb
@@ -10,8 +10,8 @@ is_dev = image_version.start_with?('99.99')
 container_name = "docserver#{'_dev' if is_dev}"
 
 # запускаем контейнер с образом соответствующей версии
-command = "docker run -i -t -d -p 80:80 #{'--restart unless-stopped ' if !is_dev}\
---env JWT_ENABLED=false --env ALLOW_PRIVATE_IP_ADDRESS=true \
+command = "docker run -i -t -d -p 80:80 #{'--restart unless-stopped ' if !is_dev} --env ALLOW_PRIVATE_IP_ADDRESS=true \
+--env JWT_ENABLED=true --env JWT_SECRET=docserver_jwt --env JWT_HEADER=Authorization \
 --volume ~/DocumentServer/logs:/var/log/onlyoffice  \
 --volume ~/DocumentServer/data:/var/www/onlyoffice/Data  \
 --volume ~/DocumentServer/lib:/var/lib/onlyoffice \
